6b88821d2697d5bf25afc905026f4013fd864417
marcossebas92-afk <marcossebas92@gmail.com>
2026-06-28 21:00:46 -0500

    Fix syntax error in script.js

--- scripts/script.js
+++ scripts/script.js
@@ -8,13 +8,14 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 const links = navMenu.querySelectorAll("a");
+    
 links.forEach(function(link) {
     link.addEventListener("click", function() {
         navMenu.classList.remove("active");
 
         });
     });
-});
+}
 
 /* about page */
 
